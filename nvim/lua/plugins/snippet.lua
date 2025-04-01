a6676ed7806d1115339afde344e88b182354974e
Julien ROUSSEAU <jrousseau@doubletrade.com>
2025-04-01 09:33:19 +0200

    chore: add ts snippet

--- nvim/lua/plugins/snippet.lua
+++ nvim/lua/plugins/snippet.lua
@@ -3,9 +3,34 @@ return {
   "L3MON4D3/LuaSnip",
   version = "v2.*",
   build = "make install_jsregexp",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   config = function()
-    local ls = require("luasnip")
-    require("luasnip.loaders.from_snipmate").lazy_load()
+    local plenary = require('plenary.scandir')
+    local cwd = vim.fn.getcwd()
+    local file = vim.fn.getcwd() .. "/.vscode/ts.code-snippets"
+
+    local files = plenary.scan_dir(cwd .. "/.vscode", {
+      depth = 2,
+      hidden = true,
+      search_pattern = ".code[-]snippets$" -- extension ".code-snippets"
+    })
+
+    if #files > 0 then
+      for _, file in ipairs(files) do
+        local text1 = file
+        local substring1 = ".code-snippets"
+
+        if string.find(text1, substring1, 0, true) == nil then
+          vim.print('The string does not contain the substring.')
+        else
+          require("luasnip.loaders.from_vscode").load_standalone({ lazy = false, path = file })
+        end
+      end
+    else
+      vim.print('No .code-snippets files found in the CWD.')
+    end
   end,
   keys = {
     {
