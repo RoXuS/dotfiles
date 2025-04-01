49a880167d1b564abf29c2660c9677f581439a2c
Julien ROUSSEAU <jrousseau@doubletrade.com>
2025-04-01 09:32:27 +0200

    chore: better lualine conf

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,24 +1,7 @@
 return {
-  'freddiehaddad/feline.nvim',
-  opts = {},
-  config = function()
-    -- Setup for catpuccin
-    local ctp_feline = require('catppuccin.groups.integrations.feline')
-    ctp_feline.setup()
-    require("feline").setup({
-      components = ctp_feline.get(),
-    })
-
-    -- Auto switch flavour
-    vim.api.nvim_create_autocmd("ColorScheme", {
-      pattern = "*",
-      callback = function()
-        package.loaded["feline"] = nil
-        package.loaded["catppuccin.groups.integrations.feline"] = nil
-        require("feline").setup {
-          components = require("catppuccin.groups.integrations.feline").get(),
-        }
-      end,
-    })
-  end
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    theme = "catppuccin"
+  }
 }
